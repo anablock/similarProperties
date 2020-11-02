4b7148161109333e450258470e10386809edb32a
Vuk <vuchina@icloud.com>
2020-11-01 16:08:44 -0800

    first commit

---
+++ force-app/main/default/lwc/similarProperties/similarProperties.js
@@ -0,0 +1,6 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import findProperties from '@salesforce/apex/SimilarPropertyController.findProperties';
+
+export default class SimilarProperties extends LightningElement {
+
+}
