2a5c09a2a48c128e107c39bd0752652f2f9a3b1a
Vuk <vuchina@icloud.com>
2020-11-02 16:14:12 -0800

    add

--- force-app/main/default/lwc/similarProperty/similarProperty.js
+++ force-app/main/default/lwc/similarProperty/similarProperty.js
@@ -1,19 +1,51 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class SimilarProperty extends NavigationMixin(LightningElement) {
     @api item;
+
+    @track editMode = false;
 
     @wire(CurrentPageReference) pageRef;
 
     navigateToRecord() {
         this[NavigationMixin.Navigate]({
             type: 'lightningCommunity__Page',
-                attributes: {
-                    recordId: this.item.Id,
-                    objectApiName: 'Property__c',
-                    actionName: 'view'
-                }
-        })
+            attributes: {
+                recordId: this.item.Id,
+                objectApiName: 'Property__c',
+                actionName: 'view'
+            }
+        });
+    }
+
+    editRecord() {
+        this.editMode = true;
+    }
+
+    handleSuccess() {
+        const evt = new ShowToastEvent({
+            title: 'Success!',
+            message: "The record has been successfully saved.",
+            variant: "success"
+        });
+        this.dispatchEvent(evt);
+        this.editMode = false;
+    }
+
+    handleError() {
+        const evt = new ShowToastEvent({
+            title: "Error!",
+            message: "An error occurred while attempting to save the record.",
+            variant: "error"
+        });
+        this.dispatchEvent(evt);
+        this.editMode = false;
+    }
+
+    handleCancel(event) {
+        this.editMode = false;
+        event.preventDefault();
     }
 }
